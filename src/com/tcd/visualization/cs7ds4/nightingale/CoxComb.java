8ede5774f699e31beb3cc1317ceead3d617233e2
bhattasi <56604792+bhattasi@users.noreply.github.com>
2020-02-05 21:04:15 +0000

    Added range slider for taking in date ranges

--- src/com/tcd/visualization/cs7ds4/nightingale/CoxComb.java
+++ src/com/tcd/visualization/cs7ds4/nightingale/CoxComb.java
@@ -1,8 +1,11 @@
 package com.tcd.visualization.cs7ds4.nightingale;
 
 import controlP5.*;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -14,6 +17,7 @@ import processing.data.*;
 import com.tcd.visualization.cs7ds4.nightingale.utils.VisualizerSettings;
 
 public class CoxComb extends PApplet {
+	private static VisualizerSettings settings;
 	private static final int SCREEN_WIDTH = 1800;
 	private static final int SCREEN_HEIGHT = 930;
 	private static Table table;
@@ -29,6 +33,7 @@ public class CoxComb extends PApplet {
 	private Toggle cbJan1,cbFeb2,cbMar3,cbApr4,cbMay5,cbJun6,cbJul7,cbAug8,cbSep9,cbOct10,cbNov11,cbDec12;
 	private Toggle cbJan13,cbFeb14,cbMar15,cbApr16,cbMay17,cbJun18,cbJul19,cbAug20,cbSep21,cbOct22,cbNov23,cbDec24;
 	private Toggle cbJan25,cbFeb26,cbMar27,cbApr28,cbMay29,cbJun30,cbJul31,cbAug32,cbSep33,cbOct34,cbNov35,cbDec36;
+	private Map<String,ArrayList> dateInfo;
 	
 	public CoxComb(String month, String year, int visualizationPeriod) {
 		this.month=month;
@@ -39,7 +44,10 @@ public class CoxComb extends PApplet {
 	public void settings() {
 		size(SCREEN_WIDTH, SCREEN_HEIGHT);
 		table = loadTable("nightingale-data.csv", "header");
+		settings = new VisualizerSettings(table);
+		dateInfo=settings.getToggleButtonsForMonths();
 	}
+	
 	
 	public void setup() {
 		f = createFont("Georgia", 12, true);
@@ -80,50 +88,32 @@ public class CoxComb extends PApplet {
 		visualizationPeriod=count;
 		System.out.println("Actual viz period: "+visualizationPeriod);
 		
-		cbJan1 = cp5.addToggle("1").setLabel("Jan").setPosition(40,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
-		cbFeb2 = cp5.addToggle("2").setLabel("Feb").setPosition(62,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
-		cbMar3 = cp5.addToggle("3").setLabel("Mar").setPosition(84,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
-		cbApr4 = cp5.addToggle("4").setLabel("Apr").setPosition(106,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
-		cbMay5 = cp5.addToggle("5").setLabel("May").setPosition(128,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
-		cbJun6 = cp5.addToggle("6").setLabel("Jun").setPosition(150,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
-		cbJul7 = cp5.addToggle("7").setLabel("Jul").setPosition(172,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
-		cbAug8 = cp5.addToggle("8").setLabel("Aug").setPosition(194,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
-		cbSep9 = cp5.addToggle("9").setLabel("Sep").setPosition(216,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
-		cbOct10 = cp5.addToggle("10").setLabel("Oct").setPosition(238,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
-		cbNov11 = cp5.addToggle("11").setLabel("Nov").setPosition(260,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
-		cbDec12 = cp5.addToggle("12").setLabel("Dec").setPosition(282,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
-		
-		cbJan13 = cp5.addToggle("13").setLabel("Jan").setPosition(304,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
-		cbFeb14 = cp5.addToggle("14").setLabel("Feb").setPosition(326,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
-		cbMar15 = cp5.addToggle("15").setLabel("Mar").setPosition(348,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
-		cbApr16 = cp5.addToggle("16").setLabel("Apr").setPosition(370,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
-		cbMay17 = cp5.addToggle("17").setLabel("May").setPosition(392,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
-		cbJun18 = cp5.addToggle("18").setLabel("Jun").setPosition(414,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
-		cbJul19 = cp5.addToggle("19").setLabel("Jul").setPosition(436,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
-		cbAug20 = cp5.addToggle("20").setLabel("Aug").setPosition(458,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
-		cbSep21 = cp5.addToggle("21").setLabel("Sep").setPosition(480,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
-		cbOct22 = cp5.addToggle("22").setLabel("Oct").setPosition(502,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
-		cbNov23 = cp5.addToggle("23").setLabel("Nov").setPosition(524,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
-		cbDec24 = cp5.addToggle("24").setLabel("Dec").setPosition(546,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
-		
-		cbJan25 = cp5.addToggle("25").setLabel("Jan").setPosition(568,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
-		cbFeb26 = cp5.addToggle("26").setLabel("Feb").setPosition(590,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
-		cbMar27 = cp5.addToggle("27").setLabel("Mar").setPosition(612,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
-		cbApr28 = cp5.addToggle("28").setLabel("Apr").setPosition(634,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
-		cbMay29 = cp5.addToggle("29").setLabel("May").setPosition(656,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
-		cbJun30 = cp5.addToggle("30").setLabel("Jun").setPosition(678,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
-		cbJul31 = cp5.addToggle("31").setLabel("Jul").setPosition(700,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
-		cbAug32 = cp5.addToggle("32").setLabel("Aug").setPosition(722,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
-		cbSep33 = cp5.addToggle("33").setLabel("Sep").setPosition(744,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
-		cbOct34 = cp5.addToggle("34").setLabel("Oct").setPosition(766,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
-		cbNov35 = cp5.addToggle("35").setLabel("Nov").setPosition(788,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
-		cbDec36 = cp5.addToggle("36").setLabel("Dec").setPosition(810,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
-		
-		
-		
-		
-		
-	}
+
+		 Range range = cp5.addRange("rangeController")
+		             // disable broadcasting since setRange and setRangeValues will trigger an event
+		             .setBroadcast(false) 
+		             .setPosition(40,height/2+348)
+		             .setSize(720,40)
+		             .setHandleSize(20)
+		             // after the initialization we turn broadcast back on again
+		             .setBroadcast(true)
+		             .setColorForeground(color(255,40))
+		             .setColorBackground(color(255,40)).setNumberOfTickMarks(24).setLabelVisible(false)
+		             ;
+
+		 int initialX=40,item=1;
+			 for(Map.Entry<String, ArrayList> entrySet : dateInfo.entrySet()) {
+				 cp5.addTextlabel("labelItem"+item++).setText(entrySet.getKey()).setPosition(initialX,height/2+415);
+				 int indexInner=0;
+				 while(indexInner<entrySet.getValue().size()) {
+					 cp5.addTextlabel("labelItem"+item++).setText(entrySet.getValue().get(indexInner).toString()).setPosition(initialX,height/2+400);
+					 initialX+=30;
+					 indexInner++;
+				 }
+			}
+		 
+		 
+	} 
 
 
 
@@ -171,7 +161,7 @@ public class CoxComb extends PApplet {
 			yearNo++;
 			rowIter++;
 		}
-	} 
+	}  
 	
 	public void mouseMoved() {
 		zygStrokeColor=255;
--- src/com/tcd/visualization/cs7ds4/nightingale/utils/VisualizerSettings.java
+++ src/com/tcd/visualization/cs7ds4/nightingale/utils/VisualizerSettings.java
@@ -1,5 +1,91 @@
 package com.tcd.visualization.cs7ds4.nightingale.utils;
 
-public abstract class VisualizerSettings {
-	public String settings="";
-}
+import java.util.Map;
+import java.util.Set;
+
+import controlP5.ControlP5;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+
+import processing.data.*;
+import processing.core.*;
+
+public  class VisualizerSettings extends PApplet {
+	private final Table table;
+	private Map<String, ArrayList> dateInfo;
+	public VisualizerSettings(Table table) {
+		this.table = table;
+	}
+	
+	public Map<String,ArrayList> getToggleButtonsForMonths(){
+		dateInfo = new LinkedHashMap<String,ArrayList>();
+		Set<String> years = new LinkedHashSet<String>();
+		for(TableRow row : table.rows()) {
+			String monthYear = row.getString("Month");
+			String year = monthYear.substring(monthYear.indexOf(' ')+1);
+			years.add(year);
+		}
+		for(String item : years) {
+			dateInfo.put(item, new ArrayList<String>());
+		}
+		for(String year : dateInfo.keySet()) {
+			for(TableRow row : table.rows()) {
+				String monthYear = row.getString("Month");
+				String extrYear = monthYear.substring(monthYear.indexOf(' ')+1);
+				String month = monthYear.substring(0,monthYear.indexOf(' '));
+				if(extrYear.equals(year)) {
+					ArrayList thisYearList = dateInfo.get(year);
+					thisYearList.add(month);
+					dateInfo.put(year,thisYearList);
+				}
+			}
+		}
+		System.out.println(dateInfo);
+		return dateInfo;
+	}
+}	
+
+/*
+cbJan1 = cp5.addToggle("1").setLabel("Jan").setPosition(40,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
+cbFeb2 = cp5.addToggle("2").setLabel("Feb").setPosition(62,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
+cbMar3 = cp5.addToggle("3").setLabel("Mar").setPosition(84,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
+cbApr4 = cp5.addToggle("4").setLabel("Apr").setPosition(106,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
+cbMay5 = cp5.addToggle("5").setLabel("May").setPosition(128,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
+cbJun6 = cp5.addToggle("6").setLabel("Jun").setPosition(150,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
+cbJul7 = cp5.addToggle("7").setLabel("Jul").setPosition(172,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
+cbAug8 = cp5.addToggle("8").setLabel("Aug").setPosition(194,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
+cbSep9 = cp5.addToggle("9").setLabel("Sep").setPosition(216,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
+cbOct10 = cp5.addToggle("10").setLabel("Oct").setPosition(238,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
+cbNov11 = cp5.addToggle("11").setLabel("Nov").setPosition(260,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
+cbDec12 = cp5.addToggle("12").setLabel("Dec").setPosition(282,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
+
+cbJan13 = cp5.addToggle("13").setLabel("Jan").setPosition(304,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
+cbFeb14 = cp5.addToggle("14").setLabel("Feb").setPosition(326,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
+cbMar15 = cp5.addToggle("15").setLabel("Mar").setPosition(348,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
+cbApr16 = cp5.addToggle("16").setLabel("Apr").setPosition(370,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
+cbMay17 = cp5.addToggle("17").setLabel("May").setPosition(392,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
+cbJun18 = cp5.addToggle("18").setLabel("Jun").setPosition(414,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
+cbJul19 = cp5.addToggle("19").setLabel("Jul").setPosition(436,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
+cbAug20 = cp5.addToggle("20").setLabel("Aug").setPosition(458,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
+cbSep21 = cp5.addToggle("21").setLabel("Sep").setPosition(480,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
+cbOct22 = cp5.addToggle("22").setLabel("Oct").setPosition(502,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
+cbNov23 = cp5.addToggle("23").setLabel("Nov").setPosition(524,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
+cbDec24 = cp5.addToggle("24").setLabel("Dec").setPosition(546,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
+
+cbJan25 = cp5.addToggle("25").setLabel("Jan").setPosition(568,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
+cbFeb26 = cp5.addToggle("26").setLabel("Feb").setPosition(590,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
+cbMar27 = cp5.addToggle("27").setLabel("Mar").setPosition(612,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
+cbApr28 = cp5.addToggle("28").setLabel("Apr").setPosition(634,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
+cbMay29 = cp5.addToggle("29").setLabel("May").setPosition(656,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
+cbJun30 = cp5.addToggle("30").setLabel("Jun").setPosition(678,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
+cbJul31 = cp5.addToggle("31").setLabel("Jul").setPosition(700,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
+cbAug32 = cp5.addToggle("32").setLabel("Aug").setPosition(722,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
+cbSep33 = cp5.addToggle("33").setLabel("Sep").setPosition(744,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
+cbOct34 = cp5.addToggle("34").setLabel("Oct").setPosition(766,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
+cbNov35 = cp5.addToggle("35").setLabel("Nov").setPosition(788,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
+cbDec36 = cp5.addToggle("36").setLabel("Dec").setPosition(810,height/2+400).setSize(20,20).setValue(true).setMode(ControlP5.CHECKBOX).setState(true);
+
+*/
