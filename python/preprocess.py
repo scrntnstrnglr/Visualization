226d084df84189418b6c8e89ee307c2f516d95aa
bhattasi <56604792+bhattasi@users.noreply.github.com>
2020-02-07 19:46:53 +0000

    code to install uninstalled packages for python

--- python/preprocess.py
+++ python/preprocess.py
@@ -1,9 +1,18 @@
 from os import listdir
 from os.path import isfile,join
 import os
-import pandas as pd
-    
+import pip
+#import pandas as pd
+
+def import_or_install(package):
+    try:
+        __import__(package)
+    except ImportError:
+        pip.main(['install', package]) 
+
 def main():
+    import_or_install("pandas")
+    import pandas as pd
     print("\n#############################Preprocessing#############################\n")
     print("Importing data set...")
     file="C:\\Users\\SIDDHARTHA\\workspace_sid\\Visualization\\nightingale-data.xlsx"
